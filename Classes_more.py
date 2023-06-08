77fa04dc3e0d72aaab0d6e52e9f029b0814cdfad
neurohunt <75321938+neurohunt@users.noreply.github.com>
2023-06-08 14:27:06 +0300

    Create Classes_more.py

---
+++ Classes_more.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self):
+        self.__balance = 0
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.__balance:
+            print("Insufficient balance!")
+            return
+        self.__balance -= amount
+
+    def check_balance(self):
+        return self.__balance
+
+account = BankAccount()
+account.deposit(100)
+account.withdraw(30)
+print(account.check_balance())  # prints: 70
